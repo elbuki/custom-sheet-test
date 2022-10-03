69243197568b551c16f3b5508668906f5014614a
Marco Carmona <elbuki@users.noreply.github.com>
2022-10-03 15:27:25 -0600

    Initial Commit

---
+++ CustomSheetTest/CustomSheetTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomSheetTestApp.swift
+//  CustomSheetTest
+//
+//  Created by Marco Carmona on 10/3/22.
+//
+
+import SwiftUI
+
+@main
+struct CustomSheetTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
